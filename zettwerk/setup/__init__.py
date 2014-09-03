7b7f9891fc0c6b72bf3da69279568a59884f3e3f
Henning Garus <hg@zettwerk.com>
2014-09-03 12:50:42 +0200

    Add a function to set the session cookie timeout

--- zettwerk/setup/__init__.py
+++ zettwerk/setup/__init__.py
@@ -7,8 +7,9 @@ handle_catalog_indexe
 from cleanup import cleanup_default_plone
 cleanup_default_plone
 
-from pas import set_pas_session_cookie_name
+from pas import set_pas_session_cookie_name, set_pas_session_cookie_timeout
 set_pas_session_cookie_name
+set_pas_session_cookie_timeout
 
 from structure import handle_structure
 handle_structure
--- zettwerk/setup/pas.py
+++ zettwerk/setup/pas.py
@@ -4,3 +4,12 @@
 def set_pas_session_cookie_name(portal, name):
     """ set the session cookie name to the given name. """
     portal.acl_users.session.manage_changeProperties(cookie_name=name)
+
+
+def set_pas_session_cookie_timeout(portal, timeout):
+    """ set the timeout (in seconds) for the session cookie.
+
+        Setting the timeout to 0 will disable the timeout check
+
+    """
+    portal.acl_users.session.manage_changeProperties(timeout=timeout)
